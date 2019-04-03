186f44668d039ce220ce1bf6dd71e66ceb158b53
Miguel Martín <pomikelor@gmail.com>
2019-04-03 18:00:04 +0200

    Create Android Project, wellcome screen and actionBar

---
+++ app/src/main/java/com/example/puzzledroid/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.puzzledroid;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar =(Toolbar) findViewById(R.id.app_action_bar);
+        setSupportActionBar(toolbar);
+
+
+}
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
